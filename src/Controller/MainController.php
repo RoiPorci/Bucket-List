60cf57d84b766af36a44a5c64b96db64d86c7f3c
RoiPorci <valerian.charron@gmail.com>
2021-03-22 12:39:09 +0100

    2nd Commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,15 @@
 namespace App\Controller;
 
 
+use Symfony\Component\Routing\Annotation\Route;
+
 class MainController
 {
-
+    /**
+     * @Route()
+     */
+    public function home(){
+        echo "<h1>Accueil</h1>";
+        die();
+    }
 }
