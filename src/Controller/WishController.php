b8ef404e8b28184c6a91f675b42a726405584ce0
RoiPorci <valerian.charron@gmail.com>
2021-03-23 17:15:14 +0100

    wishes bdd

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +12,28 @@ class WishController extends AbstractController
     /**
      * @Route("/wishes", name="wish_list")
      */
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishesLast10 = $wishRepository->findBy([], ['date_created' => 'DESC'], 10);
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishesLast10
+        ]);
     }
 
     /**
      * @Route("/wishes/detail/{id}", name="wish_detail")
      */
-    public function detail(int $id): Response
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
-        return $this->render('wish/detail.html.twig');
+        $wish = $wishRepository->find($id);
+
+        if(!$wish){
+            throw $this->createNotFoundException("Oups, ce voeu n'existe pas !");
+        }
+
+        return $this->render('wish/detail.html.twig', [
+            'wish' => $wish
+        ]);
     }
 }
