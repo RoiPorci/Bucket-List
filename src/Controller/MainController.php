9f4a52da5c7c0665c70b5ea234bab4201cbd2b8d
RoiPorci <valerian.charron@gmail.com>
2021-03-23 11:57:36 +0100

    Tout propre

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,20 +6,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends AbstractController
 {
     /**
      * @Route ("/", name="main_home")
      */
-    public function home(){
+    public function home(): Response
+    {
         return $this->render("main/home.html.twig");
     }
 
     /**
      * @Route ("/about-us", name="main_about-us")
      */
-    public function aboutUs(){
+    public function aboutUs() : Response
+    {
         return $this->render("main/about-us.html.twig");
     }
 }
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -9,10 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     /**
-     * @Route("/wish", name="wish_list")
+     * @Route("/wishes", name="wish_list")
      */
-    public function index(): Response
+    public function list(): Response
     {
         return $this->render('wish/list.html.twig');
+    }
+
+    /**
+     * @Route("/wishes/detail/{id}", name="wish_detail")
+     */
+    public function detail(int $id): Response
+    {
+        return $this->render('wish/detail.html.twig');
     }
 }
